7ac0c7e55f9f2bed503ca624a5d0e8c8cfa146f9
Dimitar1994 <130397087+Dimitar1994@users.noreply.github.com>
2023-04-10 21:44:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
+
+  function myFunction1() {
+    document.getElementById("demo").innerHTML = "здравеи свят";
+  }
+
+  function myFunction3() {
+    document.getElementById("demo").innerHTML = "как е";
+  }
