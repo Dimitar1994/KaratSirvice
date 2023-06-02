b71ab2b791178923d95cc2f388403ae4d0601e5a
Dimitar1994 <mitaka1884@gmail.com>
2023-06-02 19:42:01 +0300

    last ubdate

--- main.js
+++ main.js
@@ -2,21 +2,13 @@
 
     
 
-$(document).ready(function(){
-
-    $("#theTarget").skippr({
-
-        transition: 'slide',
-        speed: 1000,
-        easing: 'easeOutQuart',
-        navType: 'block',
-        childrenElementType: 'div',
-        arrows: true,
-        autoPlay: false,
-        autoPlayDuration: 5000,
-        keyboardOnAlways: true,
-        hidePrevious: false
-    
+ $(document).ready(function(){
+    $(".slider").bxSlider({
+        mode: 'fade',
+        speed: 1300,
+        auto: true,
+        pause: 6000
     });
+  });
 
-});    
+   
